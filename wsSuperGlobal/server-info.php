c22cbd67442ea1679dfd0ebf20e122a74f5e0515
cwooz <woosley.christopher@gmail.com>
2018-03-15 16:58:27 -0400

    current page

--- wsSuperGlobal/server-info.php
+++ wsSuperGlobal/server-info.php
@@ -4,11 +4,20 @@
   // Create Server Array
   $server = [
     'Host Server Name' => $_SERVER['SERVER_NAME'],
-    'Host Header' => $_SERVER['HTTP_HOST']
+    'Host Header' => $_SERVER['HTTP_HOST'],
+    'Server Software' => $_SERVER['SERVER_SOFTWARE'],
+    'Document Root' => $_SERVER['DOCUMENT_ROOT'],
+    'Current Page' => $_SERVER['PHP_SELF']
   ];
 
   echo $server['Host Server Name'];
+  echo '<br>';
   echo $server['Host Header'];
+  echo '<br>';
+  echo $server['Server Software'];
+  echo '<br>';
+  
+  print_r($server);
 
   // Create Client Array
 ?>
